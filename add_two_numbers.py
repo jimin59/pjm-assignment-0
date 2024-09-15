ee4ed8bdffb1dc62339968172c6d648a9a20ee90
박지민 <jimin@Jimins-MacBook-Pro.local>
2024-09-14 23:30:38 -0400

    Add script that adds two numbers

---
+++ add_two_numbers.py
@@ -0,0 +1,12 @@
+# Assignment 0: Add two numbers
+
+# Define two numbers
+num1 = 5
+num2 = 10
+
+# Calculate the sum
+sum_of_numbers = num1 + num2
+
+# Print the sum
+print(f"The sum of {num1} and {num2} is {sum_of_numbers}.")
+
